29a8f347bc1e79fb89db9ceacf7c15de349e33c6
Yakov Didenko <kogotto@gmail.com>
2026-06-01 16:27:07 +0300

    Minimal SDL project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,54 @@
-#include <iostream>
+#include <SDL3/SDL.h>
 
-int main() {
-    std::cout << "Hello" << std::endl;
-    return 0;
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+
+namespace {
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+}
+
+SDL_AppResult SDL_AppInit(void** appState, int argc, char** argv) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer(
+                "figures",
+                1024, 768,
+                0,
+                &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(
+            renderer,
+            1024, 768,
+            SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appState, SDL_Event* event) {
+    switch (event->type) {
+    case SDL_EVENT_QUIT:
+        return SDL_APP_SUCCESS;
+    case SDL_EVENT_KEY_UP:
+        switch (event->key.scancode) {
+        case SDL_SCANCODE_ESCAPE:
+            return SDL_APP_SUCCESS;
+        default:
+            return SDL_APP_CONTINUE;
+        }
+    default:
+        return SDL_APP_CONTINUE;
+    }
+}
+
+SDL_AppResult SDL_AppIterate(void* appState) {
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appState, SDL_AppResult result) {
 }
